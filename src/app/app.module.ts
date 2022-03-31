b6f96782bfe2c50951e520bf63792a70a8024385
Igor Barbashov <bis200705@gmail.com>
2022-03-31 23:05:28 +0300

    feat: generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConversationListComponent } from './components/conversation-list/conversation-list.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { ChatMessageComponent } from './components/chat-message/chat-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversationListComponent,
+    ChatComponent,
+    ChatMessageComponent
   ],
   imports: [
     BrowserModule,
